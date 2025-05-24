cb7c49d25c1698497a98960eea1f2e0e9d96b4aa
EentErt <nate.giff@gmail.com>
2025-05-24 11:23:08 -0700

    final version

--- src/main.py
+++ src/main.py
@@ -3,10 +3,11 @@ import os
 import shutil
 import re
 from markdown_to_html import markdown_to_html_node
+import sys
 
-def clean_directory():
-    if os.path.exists("public"):
-        shutil.rmtree("public")
+def clean_directory(dir):
+    if os.path.exists(dir):
+        shutil.rmtree(dir)
 
 def copy_files(current_path, public_path):
     if os.path.isfile(current_path):
@@ -42,7 +43,7 @@ def generate_page(from_path, template_path, dest_path):
             destination.write(output)
     pass
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     if os.path.isfile(dir_path_content):
         with open(dir_path_content) as file:
             markdown = file.read()
@@ -52,6 +53,8 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             template = file.read()
             output = template.replace(r"{{ Title }}", title)
             output = output.replace(r"{{ Content }}", html)
+            output = output.replace(r"href=/\"", f"href=\"{basepath}")
+            output = output.replace(r"src=/\"", f"src=\"{basepath}")
             dest_path = re.sub(r".md$", ".html", dest_dir_path)
             with open(dest_path, "w") as destination:
                 destination.write(output)
@@ -62,16 +65,20 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         for item in contents:
             new_path = os.path.join(dir_path_content, item)
             new_dest_path = os.path.join(dest_dir_path, item)
-            generate_pages_recursive(new_path, template_path, new_dest_path)
+            generate_pages_recursive(new_path, template_path, new_dest_path, basepath)
 
     pass
 
 
 
 def main():
-    clean_directory()
-    copy_files("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    clean_directory("docs")
+    basepath = sys.argv[0]
+    if basepath == "":
+        basepath = "/"
+    copy_files("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
+    
 
     
 
